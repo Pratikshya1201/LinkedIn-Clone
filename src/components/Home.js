532f9899827688eb1d8c460c5515db9c86fb211f
Pratikhya Mishra <pratikhyamishra@Pratikhyas-MacBook-Air.local>
2024-07-01 09:49:28 +0530

    left side of home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
   return (
@@ -12,9 +15,9 @@ const Home = (props) => {
         </p>
       </Section>
       <Layout>
-        <div>Left side</div>
-        <div>Main</div>
-        <div>Right side</div>
+        <Leftside />
+        <Main />
+        <Rightside />
       </Layout>
     </Container>
   );
@@ -43,7 +46,7 @@ const Section = styled.section`
     color: #0a66c2;
     font-size: 14px;
     a {
-        font-weight: 700;
+      font-weight: 700;
     }
   }
   p {
